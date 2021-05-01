5249b77590fff44e62023827bb49486dac6a49e5
MilicTG <milic.tg@gmail.com>
2021-05-01 21:29:20 +0200

    remove banner

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'My Movies',
       theme: ThemeData(primarySwatch: Colors.deepOrange),
       home: Home(),
